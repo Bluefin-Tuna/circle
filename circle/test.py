9c1c15735ad49ca65348d3e35a5e8cd01604d358
DumbassNamedTuna <50528857+Bluefin-Tuna@users.noreply.github.com>
2023-11-22 21:43:55 -0800

    Added testing for all cases (mixed plus noise level)

--- circle/test.py
+++ circle/test.py
@@ -17,12 +17,7 @@ def find_circle(model, x):
         output = model(image)
     return [round(i) for i in (x.shape[0]*output).tolist()[0]]
 
-def main():
-    
-    model = ConvNet()
-    checkpoint = torch.load('model.pth.tar')
-    model.load_state_dict(checkpoint)
-    model.eval()
+def mixed_test(model):
     
     nc = NoisyCircles(f"{DIR}/test/images.npy", f"{DIR}/test/labels.npy")
     results = []
@@ -31,7 +26,31 @@ def main():
         cp_hat = find_circle(model, img)
         results.append(iou(CircleParams(*cp), CircleParams(*cp_hat)))
     results = np.array(results)
-    print(results.mean()) # 0.9869
+    
+    print(f"Mixed Noise Test: {round(results.mean(), 4)}")
+
+def level_test(model):
+    for nl in range(0, 10):
+        nl /= 10
+        nc = [next(generate_examples(nl, min_radius=5, max_radius=50)) for _ in range(1000)]
+        results = []
+        for idx in range(len(nc)):
+            img, cp = nc[idx]
+            cp_hat = find_circle(model, img)
+            results.append(iou(cp, CircleParams(*cp_hat)))
+        results = np.array(results)
+        print(f"{round(nl, 2)} Noise Test: {round(results.mean(), 4)}")
+
+def main():
+    
+    model = ConvNet()
+    checkpoint = torch.load('model.pth.tar', map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint)
+    model.eval()
+    
+    mixed_test(model)
+    print()
+    level_test(model)
 
 if __name__ == "__main__":
     main()
